f64c111ef09e66bfd1a204e5e2fb459ec1b1289c
Sergey <Pcepera@hotmail.ru>
2025-10-19 11:42:53 +0300

    script to make admin user

---
+++ create_admin.py
@@ -0,0 +1,51 @@
+import psycopg2
+from psycopg2 import sql
+from werkzeug.security import generate_password_hash
+
+# Settings
+DB_NAME = "cinema_db"
+DB_USER = "your_username"
+DB_PASSWORD = "your_password"   # enter your password
+DB_HOST = "localhost"
+DB_PORT = "5432"
+
+# Admin
+admin_username = "admin"
+admin_password = "1234"
+admin_role = "admin"
+
+try:
+    conn = psycopg2.connect(
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        host=DB_HOST,
+        port=DB_PORT
+    )
+    cur = conn.cursor()
+
+    # Check admin name
+    cur.execute("SELECT * FROM users WHERE username = %s;", (admin_username,))
+    existing_user = cur.fetchone()
+
+    if existing_user:
+        print(f"User '{admin_username}' already exists.")
+    else:
+        hashed_password = generate_password_hash(admin_password)
+
+        # Insert the new admin into the table
+        cur.execute("""
+                INSERT INTO users (username, password, role)
+                VALUES (%s, %s, %s);
+            """, (admin_username, hashed_password, admin_role))
+
+        conn.commit()
+        print(f"Admin '{admin_username}' created successfully!")
+
+except Exception as e:
+    print("Database error:", e)
+
+finally:
+    if conn:
+        cur.close()
+        conn.close()
